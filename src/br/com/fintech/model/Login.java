10b221666b4ece1fbbc157161f7ff4e4d613c683
Willder Canabrava <willcanabrava@gmail.com>
2024-08-16 21:02:12 -0300

    adicionando projeto

--- src/br/com/fintech/model/Login.java
+++ src/br/com/fintech/model/Login.java
@@ -1,2 +1,43 @@
-package br.com.fintech.model;public class Login {
+package br.com.fintech.model;
+
+import java.util.Scanner;
+
+public class Login {
+
+    Scanner sc = new Scanner(System.in);
+
+    private String username;
+    private String password;
+    int count = 1;
+
+    public Login() {
+    }
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void showAcessScreen() {
+        System.out.println("Username: ");
+        username = sc.nextLine();
+        System.out.println("Senha: ");
+        password = sc.nextLine();
+    }
+
+
+    public void accessLogin() {
+        while(!username.equals("luissouza@fintech.com") && password.equals("Fintech1234!") && count <= 2) {
+            System.out.println("Username ou senha inválido! Você tem mais " + (3 - count) + "tentativas.");
+            count++;
+            username = sc.nextLine();
+            password = sc.nextLine();
+        }
+        if (username.equals("luissouza@fintech.com") && (password.equals("Fintech1234!"))) {
+
+            System.out.println("Acesso autorizado!");
+        } else {
+            System.out.println("Acesso bloqueado!");
+        }
+    }
 }
